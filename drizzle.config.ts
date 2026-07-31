b423794b287d63bead12c0543e02f0255a7aa962
Tapish19 <tapish199@gmail.com>
2026-07-31 14:24:33 +0530

    Update dbCredentials to use environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,12 @@ export default defineConfig({
   schema: "./src/lib/api/db/schema.ts",
   out: "./drizzle",
   dialect: "turso",
-  dbCredentials: {
-    url: "file:./pg-one.db",
-  },
+  dbCredentials: process.env.TURSO_DATABASE_URL
+    ? {
+        url: process.env.TURSO_DATABASE_URL,
+        authToken: process.env.TURSO_AUTH_TOKEN,
+      }
+    : {
+        url: "file:./pg-one.db",
+      },
 });
